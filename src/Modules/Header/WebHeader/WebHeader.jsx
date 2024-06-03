57957919f6c21632d2f55f4269c38b6c22f85e1d
Nazariy Belelya <belelya.nazar1999@gmail.com>
2024-06-03 09:56:20 +0300

    add languages

--- src/Modules/Header/WebHeader/WebHeader.jsx
+++ src/Modules/Header/WebHeader/WebHeader.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import styles from './WebHeader.module.css'
 import { Link } from 'react-scroll'
-import _ from 'lodash'
 import { logo } from '../../../Assets/Images'
 import { CustomButton, LangDetector } from '../../../Components'
 import { useTranslation } from 'react-i18next'
@@ -10,42 +9,8 @@ import { btnArrow } from '../../../Assets/Icons'
 const WebHeader = () => {
   const [t] = useTranslation()
 
-  const [show, setShow] = useState(null)
-
-  const controlNavBar = _.throttle(() => {
-    const currentScrollY = window.scrollY
-    if (lastScrollY < currentScrollY) {
-      setShow('true')
-    } else {
-      setShow('false')
-    }
-    lastScrollY = currentScrollY
-  }, 200)
-
-  let lastScrollY = window.scrollY
-
-  useEffect(() => {
-    const handleScroll = () => {
-      controlNavBar()
-    }
-
-    window.addEventListener('scroll', handleScroll)
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  })
-
   return (
-    <header
-      className={`${styles.wrap} ${
-        show === 'false'
-          ? styles.wrapShow
-          : show === 'true'
-          ? styles.wrapHidden
-          : null
-      }`}
-    >
+    <header className={styles.wrap}>
       <div className={`customContainer ${styles.container}`}>
         <img src={logo} alt="logo" className={styles.container_logo} />
         <div className={styles.container_wrap}>
