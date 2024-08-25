06c57239729c21ea97820cbc207fff8d58bca3c7
Nazariy Belelya <belelya.nazar1999@gmail.com>
2024-08-25 17:18:44 +0300

    done pravky

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom'
 //import ReCAPTCHA from 'react-google-recaptcha'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 import './App.css'
 import { AppRouter, ScrollToTop } from './Components'
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <ScrollToTop />
+        <SpeedInsights />
         <AppRouter />
       </BrowserRouter>
       {/*<ReCAPTCHA*/}
