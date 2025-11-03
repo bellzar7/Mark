657dc2b3fa31a37ea5fe552f83f6b19538a9acba
bellzar7 <belelya.nazar1999@gmail.com>
2025-11-03 17:40:22 +0200

    update contact information in socials.js

--- src/Constants/socials.js
+++ src/Constants/socials.js
@@ -1,8 +1,10 @@
-const mail = 'hello@markdev.agency'
+// const mail = 'hello@markdev.agency'
+const mail = 'markdevadmin@proton.me'
 const instLink = 'https://www.instagram.com/mark.itdev'
 const tgLink = 'https://t.me/markitdev'
 const watsappLink = 'https://wa.me/+380636148616'
-const tel = '+38 (063) 614-86-16'
+// const tel = '+38 (063) 614-86-16'
+const tel = '+38 (097) 309-92-00'
 const linkedinLink =
   'https://www.linkedin.com/company/mark-web-development/about/'
 
